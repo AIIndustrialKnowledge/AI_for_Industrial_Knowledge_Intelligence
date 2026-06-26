39fc7425d54288c893eab935c2606c7fa5fa58a3
Hemanth12572 <hemanthkumarrallapalli@gmail.com>
2026-06-26 21:30:57 +0530

    Implemented user registration backend with Spring Boot

--- backend/src/main/java/com/aiindustrial/backend/entity/User.java
+++ backend/src/main/java/com/aiindustrial/backend/entity/User.java
@@ -10,13 +10,16 @@ public class User {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @Column(nullable = false)
     private String fullName;
 
-    @Column(unique = true)
+    @Column(nullable = false, unique = true)
     private String email;
 
+    @Column(nullable = false)
     private String password;
 
+    @Column(nullable = false)
     private String role;
 
     public User() {
@@ -61,4 +64,5 @@ public class User {
     public void setRole(String role) {
         this.role = role;
     }
+
 }
